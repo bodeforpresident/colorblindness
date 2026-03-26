92d1d1607d080318dc8d81efe4c75f1840c29b6e
Bode <289588@burlcoschools.org>
2026-03-26 09:46:44 -0400

    Update colorblindnesscalculator.cpp

--- colorblindnesscalculator.cpp
+++ colorblindnesscalculator.cpp
@@ -23,7 +23,7 @@ void inputs(int& mother, int& father) {
     cout << "To use: Follow the instructions that appear on screen.\n\n";
     
     // Input Section, Mother
-    cout << "If the mother is not colorblind, enter zero.\nIf the mother is colorblind, enter one.\nIf the mother is a carrier for colorblindness, enter two.\n";
+    cout << "If the mother is not colorblind, enter 1.\nIf the mother is colorblind, enter 2.\nIf the mother is a carrier for colorblindness, enter 3.\n";
     cin >> mother;
     
     // Validation
@@ -33,13 +33,13 @@ void inputs(int& mother, int& father) {
         cin.ignore(10000, '\n');
         cin >> mother;
     }
-    while (mother < 0 || mother > 3) {
+    while (mother < 1 || mother > 4) {
         cout << "Please enter zero, one, or two.\n";
         cin >> mother;
     }
     
     // Input Section, Father
-    cout << "If the father is not colorblind, enter zero.\nIf the father is colorblind, enter one.\n";
+    cout << "If the father is not colorblind, enter 1.\nIf the father is colorblind, enter 2.\n";
     cin >> father;
     
     // Validation
@@ -49,7 +49,7 @@ void inputs(int& mother, int& father) {
         cin.ignore(10000, '\n');
         cin >> father;
     }
-    while (father < 0 || father > 1) {
+    while (father < 1 || father > 2) {
         cout << "Please enter zero, one, or two.\n";
         cin >> father;
     }    
@@ -58,7 +58,7 @@ void inputs(int& mother, int& father) {
 // Punnett square functions
 void squares(int& mother, int& father, int& loop, char& keyIn) {
     cout << "This is your punnet square:\n\n";
-    if (mother == 0 & father == 0) { // First punnett square
+    if (mother == 1 & father == 1) { // First punnett square
         cout << "    |   XB    |    XB   |\n";
         cout << "=========================\n";
         cout << "XB  |XBXB     |XBXB     |\n";
@@ -66,7 +66,7 @@ void squares(int& mother, int& father, int& loop, char& keyIn) {
         cout << "Y   |XBY      |XBY      |\n";
         cout << "=========================\n\n";
         cout << "Chance of a colorblind child: 0%\n";
-    } else if (mother == 1 & father == 0) { // Second punnett square
+    } else if (mother == 2 & father == 1) { // Second punnett square
         cout << "    |   Xb    |    Xb   |\n";
         cout << "=========================\n";
         cout << "XB  |XBXb     |XBXb     |\n";
@@ -75,7 +75,7 @@ void squares(int& mother, int& father, int& loop, char& keyIn) {
         cout << "=========================\n\n";
         cout << "Chance of a colorblind child: 0%\n";
         cout << "Chance of carriers for colorblindness: 100%\n";
-    } else if (mother == 2 & father == 0) { // Third punnett square
+    } else if (mother == 3 & father == 1) { // Third punnett square
         cout << "    |   XB    |    Xb   |\n";
         cout << "=========================\n";
         cout << "XB  |XBXB     |XBXb     |\n";
@@ -84,7 +84,7 @@ void squares(int& mother, int& father, int& loop, char& keyIn) {
         cout << "=========================\n\n";  
         cout << "Chance of a colorblind child: 25%\n";
         cout << "Chance of carriers for colorblindness: 25%\n";
-    } else if (mother == 0 & father == 1) { // Fourth punnett square
+    } else if (mother == 1 & father == 2) { // Fourth punnett square
         cout << "    |   XB    |    XB   |\n";
         cout << "=========================\n";
         cout << "Xb  |XBXb     |XBXb     |\n";
@@ -93,7 +93,7 @@ void squares(int& mother, int& father, int& loop, char& keyIn) {
         cout << "=========================\n\n";
         cout << "Chance of a colorblind child: 0%\n";
         cout << "Chance of carriers for colorblindness: 50%\n";
-    } else if (mother == 1 & father == 1) { // Fifth punnett square
+    } else if (mother == 2 & father == 2) { // Fifth punnett square
         cout << "    |   Xb    |    Xb   |\n";
         cout << "=========================\n";
         cout << "Xb  |XbXb     |XbXb     |\n";
@@ -101,7 +101,7 @@ void squares(int& mother, int& father, int& loop, char& keyIn) {
         cout << "Y   |XbY      |XbY      |\n";
         cout << "=========================\n\n";
         cout << "Chance of a colorblind child: 100%\n";
-    } else if (mother == 2 & father == 1) { // Sixth punnett square
+    } else if (mother == 3 & father == 2) { // Sixth punnett square
         cout << "    |   XB    |    Xb   |\n";
         cout << "=========================\n";
         cout << "Xb  |XBXb     |XbXb     |\n";
